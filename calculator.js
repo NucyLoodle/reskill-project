13ccde1aefddd6ba3ba04f2266a589f3bd331456
NucyLoodle <lucymeredith3@gmail.com>
2024-08-02 14:59:44 +0100

    compute calculation and update display

--- calculator.js
+++ calculator.js
@@ -8,19 +8,23 @@ const buttons = document.querySelectorAll('button');
 // change calculator display
 function button_pressed() {
     //console.log(item);
-    
+    // gets id from each button
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            // update display by replacing the 0 value
+            if (display.textContent === '0') {
+                display.textContent = button.textContent
+            // update display with operations
+            } else if (button.textContent != '=') {
+                display.textContent += button.textContent
+            } else {
+                calculation = display.textContent
+                solution = eval(calculation)
+                display.textContent = solution
+            }
+            
+        });
+    });
 }
 
-// gets id from each button
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        if (display.textContent === '0') {
-            display.textContent = button.textContent
-        } else {
-            display.textContent += button.textContent
-        }
-        
-    });
-});
-
-
+button_pressed()
