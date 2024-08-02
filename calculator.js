2f3fd7be32139fea8fa88a987080fad2e6dab130
NucyLoodle <lucymeredith3@gmail.com>
2024-08-02 20:10:42 +0100

    handle errors

--- calculator.js
+++ calculator.js
@@ -1,8 +1,6 @@
-const container = document.querySelector("operations");
+const operations = document.querySelector("operations");
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll('button');
-
-
 
 // performs an action when button is clicked
 // change calculator display
@@ -11,20 +9,26 @@ function button_pressed() {
     // gets id from each button
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
+            
             // update display by replacing the 0 value
             if (display.textContent === '0') {
                 display.textContent = button.textContent
             // update display with operations
             } else if (button.textContent != '=') {
                 display.textContent += button.textContent
+            // prevent user from entering more than one operation at a time
+            // pass
             } else {
                 calculation = display.textContent
-                solution = eval(calculation)
-                display.textContent = solution
-            }
-            
+                try {
+                    
+                    solution = eval(calculation)
+                    display.textContent = solution                    
+                } catch (error) {
+                    display.textContent = "This is not a valid calculation."                    
+                }   
+            }           
         });
     });
 }
-
 button_pressed()
