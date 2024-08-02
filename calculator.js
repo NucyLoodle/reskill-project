9414aa8b2285e131ed0dd637c6f4bc26a44a5d37
NucyLoodle <lucymeredith3@gmail.com>
2024-08-02 09:12:31 +0100

    update display with user input

--- calculator.js
+++ calculator.js
@@ -1,15 +1,24 @@
 const container = document.querySelector("operations");
-const display = document.createElement('div');
-
+const display = document.querySelector(".display");
 const buttons = document.querySelectorAll('button');
+let currentNum = display.textContent
 
+
+// performs an action when button is clicked
+// change calculator display
+function button_pressed(item) {
+    console.log(item);
+    if (currentNum === '0') {
+        display.textContent = item
+    }
+}
+
+// gets id from each button
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         button_pressed(button.textContent)
-    
+        
     });
 });
 
-function button_pressed(item) {
-    console.log(item);
-}
+
