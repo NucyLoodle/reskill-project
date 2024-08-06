071dab1842034b6a6eb5db107412a908af021131
NucyLoodle <lucymeredith3@gmail.com>
2024-08-06 10:21:09 +0100

    write test for adding method

--- python-calculator/test_calculator.py
+++ python-calculator/test_calculator.py
@@ -1,5 +1,15 @@
 import unittest
 from unittest import TestCase
+from calculator import Calculator
+calculator = Calculator()
+
+class TestCalculator(TestCase):
+
+    def test_add(self):
+        result = calculator.add(3, 5)
+        expected = 8
+        self.assertEqual(expected, result)
+
 
 if __name__ == '__main__':
     unittest.main()
